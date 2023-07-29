c8d1ff6f0d8812bb4c5246a5713e5cd4b72ac0d4
Dirk Christian Becker <dirk.christian.becker@gmail.com>
2023-07-29 16:17:21 +0200

    Changed build system to cmake

---
+++ Tests/missing_overload_error.cpp
@@ -0,0 +1,13 @@
+#include "../Source/unit_test_framework.h"
+struct A {
+  bool operator==(const A &rhs) const {
+    return true;
+  }
+};
+
+TEST(foo) {
+  ASSERT_EQUAL(A(), A());
+  ASSERT_NOT_EQUAL(A(), A());
+}
+
+TEST_MAIN()
--- test/test_name_conflict_file_with_non_static_func.cpp
+++
@@ -1,3 +0,0 @@
-// Function name conflicts with a test case in
-// test_name_conflict_file_with_tests.cpp.
-void my_test() {}
